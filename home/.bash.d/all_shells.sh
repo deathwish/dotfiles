c29d4269729c93277315c5e659a48ccaee2448ea
Lance Cooper <lance.m.cooper@gmail.com>
2013-05-17 15:20:47 +0200

    Fix some shell path issues and set GEM_HOME up correctly.

--- home/.bash.d/all_shells.sh
+++ home/.bash.d/all_shells.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+export PATH="/bin:/usr/bin:/usr/local/bin"
+
+export GEM_HOME="${HOME}/.gem"
+mkdir -p "${GEM_HOME}"
+export PATH="${PATH}:${GEM_HOME}/bin"
+
 # Load RVM into a shell session *as a function*
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
--- home/.bash.d/linux/exports.sh
+++ home/.bash.d/linux/exports.sh
@@ -2,5 +2,5 @@
 
 #some things mono related don't work without this
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/lib"
-export PATH="${PATH}:/var/lib/gems/1.8/bin"
+export PATH="${PATH}:/var/lib/gems/1.8/bin:/usr/games"
 export SDL_AUDIODRIVER='alsa'
